2202c443557f6ca0530849aa3ac09ba09b874e12
srinivas <kamadasrinivas@yahoo.com>
2018-06-22 09:54:35 +0530

    my first commit

---
+++ src/main/java/com/techsri/demo/DemoController.java
@@ -0,0 +1,36 @@
+package com.techsri.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping(value="/createTable",method=RequestMethod.GET)
+	public void createTable(){
+		try{
+			jdbcTemplate.execute("create table CUSTOMERDATA ("+"id SERIAL, name VARCHAR2(255)"+")");
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
+	}
+	
+	@RequestMapping(value="/addData",method=RequestMethod.GET)
+	public void addData(){
+		
+		try{
+			jdbcTemplate.execute("insert into CUSTOMERDATA(id,name) values(1,'srinivas')" );
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/techsri/demo/SpringH2Application.java
@@ -0,0 +1,12 @@
+package com.techsri.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2Application.class, args);
+	}
+}
